40c7ae4e30ab83deefa68f20fdaf53f77289c099
Trương Thanh Tú <60811767+truong-thanh-tu@users.noreply.github.com>
2020-03-14 09:27:35 +0700

    Bai tap buoi dau tien

---
+++ src/session/session_3.java
@@ -0,0 +1,117 @@
+package session;
+import java.util.Scanner;
+public class session_3 {
+//	public static void main(String[] args) {
+//	    int first = 400, second = 700, result;       
+//	    result = first + second;
+//	     if (result > 1000) {           
+//	    	 second = second + 100;     
+//	    	 }; 
+//	     System.out.print("Second " + second);
+//	}
+	
+//	  public static void main(String[] args) { 
+//		  int first = 400, second = 700, result;  
+//		  result = first + second; 
+//		  if (result > 1000)  
+//		  second = second + 100;
+//		  System.out.println("The value of second is " + second); 
+//	  }
+
+//	 public static void main(String[] args) { 
+//	       int number = 11, remainder;
+//	       remainder = number % 2;
+//	       if (remainder == 0) {
+//	    	   System.out.println("Number is even");
+//	    	   }
+//	       else {
+//	    	   System.out.println("Number is odd");
+//	    	   } 
+//	 }
+	
+//	public static void main(String[] args) {
+//	     Scanner input = new Scanner(System.in);
+//	     System.out.println("Enter a Number: ");
+//	     int num = input.nextInt();
+//	     // Checks whether a number is divisible by 3 
+//	     if (num % 3 == 0) {
+//	    	 System.out.println("Inside Outer if Block");
+//	    	 // Inner if statement checks if number is divisible by 5
+//	    	 if (num % 5 == 0) { 
+//	    	 System.out.println("Number is divisible by 3 and 5");
+//	    	 } else {
+//	    	 System.out.println("Number is divisible by 3, but not by 5");
+//	    	 } // End of inner if-else statement
+//          }
+//	     else {
+//	    	 System.out.println("Number is not divisible by 3");
+//	    	 } // End of outer if-else statement              
+//	     }
+	
+//    public static void main(String[] args) {
+//    	int totalMarks = 59; 
+//        if (totalMarks >= 90) 
+//        {
+//        	System.out.println("Grade = A+");
+//        } 
+//        else if (totalMarks >= 60) 
+//        {
+//           System.out.println("Grade = A");
+//        } 
+//        else if (totalMarks >= 40) 
+//        {
+//    	  System.out.println("Grade = B");
+//          } 
+//       	else if (totalMarks >= 30)
+//       	{ 
+//        	System.out.println("Grade = C");
+//        			
+//       	} 
+//        		
+//       	else { 
+//        			
+//       		System.out.println("Fail"); 
+//        			} 
+//    }
+
+//    public static void main(String[] args) {
+//        int choice = 3; 
+//        switch (choice) 
+//        {
+//        case 1:
+//        	System.out.println("Addition");
+//        	break;
+//        case 2:
+//        	System.out.println("Subtraction");
+//        	break;
+//        case 3:
+//        	System.out.println("Multiplication");
+//        	break;
+//        case 4:
+//        	System.out.println("Division");
+//        	break;
+//        default:
+//        	System.out.println("Invalid Choice");
+//        	}
+//       
+//        }
+	
+//    public static void main(String[] args) { 
+//    	   String day = "Monday";
+//    	   String hour = "am";
+//    	   switch (day) {
+//    	   case "Sunday":
+//    		   System.out.println("Sunday is a Holiday...");
+//    		   switch (hour) {
+//    		   case "am":
+//    			   System.out.println("Good Morning");
+//    			   break;
+//    		   case "pm":
+//    			   System.out.println("Good Evening");
+//    			   break;
+//    			  }
+//    	   }
+//    	  }
+//    }
+
+
